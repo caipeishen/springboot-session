08f6102c363fd6c88a13773028b273be8e92eed9
Mr丶Cps <peishen.cai@foxmail.com>
2020-03-26 13:55:06 +0800

    maven优化 dependencyManagement

--- springboot-session-demo1/src/main/java/cn/cps/controller/SessionController.java
+++ springboot-session-demo1/src/main/java/cn/cps/controller/SessionController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
